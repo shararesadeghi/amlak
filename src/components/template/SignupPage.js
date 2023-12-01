67facfbb5d4b95da3feb6da220258d9fdfff8d54
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-12-01 16:56:33 +0330

    npm i react-hot-toast

--- src/components/template/SignupPage.js
+++ src/components/template/SignupPage.js
@@ -2,6 +2,7 @@
 import styles from '@/template/SignupPage.module.css'
 import Link from 'next/link';
 import { useState } from 'react';
+import { Toaster } from 'react-hot-toast';
 
 const SignupPage = () => {
 
@@ -12,18 +13,18 @@ const SignupPage = () => {
         <div className={styles.form}>
             <h4>فرم ثبت نام</h4>
             <form>
-                <label>نام کاربری:</label>
+                <label>ایمیل :</label>
                 <input type="text" value={email} onChange={e=>setEmail(e.target.value)}/>
                 <label>رمز عبور:</label>
                 <input type="password" value={password} onChange={e=>setPassword(e.target.value)}/>
                 <label>تکرار رمز عبور: </label>
                 <input type="password" value={rePassword} onChange={e=>setRePassword(e.target.value)}/>
-                <button>ورود</button>
+                <button type="submit">ثبت نام</button>
             </form>
                 <p>حساب کاربری دارید؟ 
                 <Link href="/signin">ورود</Link>
                 </p>
-            
+            <Toaster/>
         </div>
     );
 };
