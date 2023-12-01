764d7da990bb4a7b7cfcf722c470741f4d0851af
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-12-01 16:22:44 +0330

    create [...nextauth] functions

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,37 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import connectDB from "@/utils/connectDB";
+import { verifyPassword } from "@/utils/auth";
+import User from "@/models/User";
+
+const authOptions = {
+  session: { strategy: "jwt" },
+  providers: [
+    CredentialsProvider({
+      async authorize(credentials) {
+        const { email, password } = credentials;
+
+        try {
+          await connectDB();
+        } catch (error) {
+          throw new Error(" مشکلی در سرور رخ داده است ");
+        }
+
+        if (!email || !password)
+          throw new Error("لطفا اطلاعات معتبر وارد کنید");
+
+        const user = await User.findOne({ email });
+        if (!user) throw new Error("لطفا ابتدا حساب کاربری ایجاد کنید");
+
+        const isValid = await verifyPassword(password, user.password);
+        if (!isValid) throw new Error("ایمیل یا پسورد اشتباه است");
+
+        return { email };
+      },
+    }),
+  ],
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
