386287a15ffbcf543dffd3ca2b2fc08303a80fd4
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-12-04 01:01:08 +0330

    change function name

--- src/components/template/DashboardPage.js
+++ src/components/template/DashboardPage.js
@@ -7,7 +7,7 @@ const DashboardPage = ({createdAt}) => {
       <p>آگهی های خود را ثبت کنید تا هزاران نفر آن را مشاهده کنند</p>
       <div className={styles.createdAt}>
           <p>تاریخ عضویت:</p>
-          <span>{new Date(createdAt).toLocalString("fa-IR")}</span>
+          <span>{new Date(createdAt).toLocaleDateString("fa-IR")}</span>
       </div>
     </div>
   );
