da9884819560eeaf23f20afb3346580813338512
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-12-04 00:58:04 +0330

    add persian date to dashboard

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,7 +1,16 @@
+import { getServerSession } from "next-auth";
+import {authOptions} from '@/app/api/auth/[...nextauth]/route';
+import connectDB from '@/utils/connectDB';
+import User from '@/models/User';
+
 import DashboardPage from "@/template/DashboardPage";
 
-const Dashboard = () => {
-  return <DashboardPage />;
+const Dashboard = async () => {
+    await connectDB();
+    const session = await getServerSession(authOptions);
+    const user = await User.findOne({email:session.user.email});
+
+  return <DashboardPage createdAt={user.createdAt}/>;
 };
 
 export default Dashboard;
