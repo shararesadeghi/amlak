c55e7090067ee065b103a35a6c80186e309f9790
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-12-04 16:44:07 +0330

    add textinput compnent to addprofilepage

--- src/components/module/TextInput.js
+++ src/components/module/TextInput.js
@@ -1,16 +1,16 @@
-"use client";
+import {p2e} from '@/utils/replaceNumber';
 import styles from'@/module/TextInput.module.css';
 
-const TextInput = ({title,name,profileData,setProfileData,textArea = false}) => {
+const TextInput = ({title,name,profileData,setProfileData,textarea = false}) => {
     const changeHandler=(e)=>{
         const {name,value} = e.target;
-        setProfileData({...profileData,[name]:[value]});
+        setProfileData({...profileData,[name]:p2e(value)});
     }
     return (
         <div className={styles.container}>
             <p>{title}:</p>
-            {textArea ?
-             <textArea tyep="text" name={name} value={profileData[name]} onChange={changeHandler}/> : <input type="text" name={name} value={profileData[name]} onChange={changeHandler}/>}
+            {textarea ?
+             <textarea tyep="text" name={name} value={profileData[name]} onChange={changeHandler}/> : <input type="text" name={name} value={profileData[name]} onChange={changeHandler}/>}
         </div>
     );
 };
--- src/components/template/AddProfilePage.js
+++ src/components/template/AddProfilePage.js
@@ -1,5 +1,6 @@
 "use client";
 import {useState} from 'react';
+import TextInput from '@/module/TextInput';
 import styles from '@/template/AddProfilePage.module.css';
 
 const AddProfilePage = () => {
@@ -16,9 +17,19 @@ const AddProfilePage = () => {
         amenities:[],
 
     })
+    const submitHandler=()=>{
+
+    }
     return (
-        <div>
-            
+        <div className={styles.container}>
+        <h3>ثبت آگهی</h3> 
+        <TextInput title="عنوان آگهی" name="title" profileData={profileData} setProfileData={setProfileData}/>
+        <TextInput title=" توضیحات" name="description" profileData={profileData} setProfileData={setProfileData} textarea={true}/>
+         <TextInput title="آدرس " name="location" profileData={profileData} setProfileData={setProfileData}/> 
+         <TextInput title="شماره تماس " name="phone" profileData={profileData} setProfileData={setProfileData}/>  
+         <TextInput title=" قیمت(تومان)" name="price" profileData={profileData} setProfileData={setProfileData}/>
+         <TextInput title=" بنگاه" name="realState" profileData={profileData} setProfileData={setProfileData}/>
+         <button className={styles.submit} onClick={submitHandler}>ثبت آگهی</button>
         </div>
     );
 };
