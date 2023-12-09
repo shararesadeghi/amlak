721078b95690416a9db5803c2fd7e5ea4e3b55d1
Jamal Shamsi <93354040+jamalshamsi1988@users.noreply.github.com>
2023-12-09 17:34:05 +0330

    add post api for profile

---
+++ src/app/api/profile/route.js
@@ -0,0 +1,84 @@
+import Profile from "@/models/Profile";
+import User from "@/models/User";
+import connectDB from "@/utils/connectDB";
+import { Types } from "mongoose";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+
+export async function POST(req) {
+  try {
+    await connectDB();
+
+    const body = await req.json();
+    const {
+      title,
+      description,
+      location,
+      phone,
+      price,
+      realState,
+      constructionDate,
+      category,
+      rules,
+      amenities,
+    } = body;
+
+    const session = await getServerSession(req);
+    if (!session) {
+      return NextResponse.json(
+        { error: "لطفا وارد حساب کاربری خود شوید" },
+        { status: 401 }
+      );
+    }
+
+    const user = await User.findOne({ email: session.user.email });
+    if (!user) {
+      return NextResponse.json(
+        { error: "حساب کاربری یافت نشد" },
+        { status: 404 }
+      );
+    }
+
+    if (
+      !title ||
+      !description ||
+      !location ||
+      !phone ||
+      !price ||
+      !realState ||
+      !constructionDate ||
+      !category
+    ) {
+      return NextResponse.json(
+        { error: "لطفا اطلاعات معتبر وارد کنید" },
+        { status: 400 }
+      );
+    }
+
+    const newProfile = await Profile.create({
+      title,
+      description,
+      location,
+      phone,
+      price: +price,
+      realState,
+      constructionDate,
+      category,
+      rules,
+      amenities,
+      userId: new Types.ObjectId(user._id),
+    });
+
+    console.log(newProfile);
+    return NextResponse.json(
+      { message: "آگهی جدید اضافه شد" },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { error: "مشکلی در سرور رخ داده است" },
+      { status: 500 }
+    );
+  }
+}
