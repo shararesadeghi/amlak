3e7369bace1078d750c99b3a43c191f134547327
Jamal Shamsi <93354040+jamalshamsi1988@users.noreply.github.com>
2023-12-11 16:21:08 +0330

    get profiles for a spacial user from api

---
+++ src/app/dashboard/my-profiles/page.js
@@ -0,0 +1,23 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import User from "@/models/User";
+import connectDB from "@/utils/connectDB";
+import { getServerSession } from "next-auth";
+
+
+ const MyProfiles = async () => {
+    await connectDB();
+    const session = await getServerSession(authOptions);
+
+    const user = await User.aggregate([{$match:{email:session.user.email}},{$lookup:{
+        from:"profiles",
+        foreignField:"userId",
+        localField:"_id",
+        as:"profiles"
+
+    }}])
+  return (
+    <div>MyProfiles</div>
+  )
+}
+
+export default MyProfiles;
