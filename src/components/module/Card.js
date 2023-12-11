f02dfd4e94c565a8e8ad7319379a970254946548
Jamal Shamsi <93354040+jamalshamsi1988@users.noreply.github.com>
2023-12-12 00:04:34 +0330

    card component for show profiles added

--- src/components/module/Card.js
+++ src/components/module/Card.js
@@ -1,9 +1,36 @@
+import { RiHome3Line } from "react-icons/ri";
+import { MdApartment } from "react-icons/md";
+import { BiStore } from "react-icons/bi";
+import { GiOfficeChair } from "react-icons/gi";
+import { HiOutlineLocationMarker } from "react-icons/hi";
+import { BiLeftArrowAlt } from "react-icons/bi";
+import styles from "@/module/Card.module.css";
+import Link from "next/link";
+import { sp } from "@/utils/replaceNumber";
 
+const Card = ({ data: { category, title, price, location } }) => {
+  const icons = {
+    villa: <RiHome3Line />,
+    apartment: <MdApartment />,
+    store: <BiStore />,
+    office: <GiOfficeChair />,
+  };
 
- const Card = ({data}) => {
   return (
-    <div>{data.title}</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.icon}>{icons[category]}</div>
+      <p className={styles.title}>{title}</p>
+      <p className={styles.location}>
+        <HiOutlineLocationMarker />
+        {location}
+      </p>
+      <span>{sp(price)} تومان</span>
+      <Link href="/">
+        مشاهده آگهی
+        <BiLeftArrowAlt />
+      </Link>
+    </div>
+  );
+};
 
 export default Card;
