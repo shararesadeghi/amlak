e5b00b4d4d3e6c2bb94b82aff2ea286ac569374b
Jamal Shamsi <93354040+jamalshamsi1988@users.noreply.github.com>
2023-12-12 16:32:25 +0330

    create delete api for profile

---
+++ src/app/api/profile/delete/[profileId]/route.js
@@ -0,0 +1,50 @@
+import { NextResponse } from "next/server";
+import connectDB from "@/utils/connectDB";
+import { getServerSession } from "next-auth";
+import User from "@/models/User";
+import Profile from "@/models/Profile";
+
+export async function DELETE(req, context) {
+  try {
+    await connectDB();
+
+    const id = context.params.profileId;
+
+    const session = await getServerSession(req);
+    if (!session) {
+      return NextResponse.json(
+        { error: "لطفا وارد حساب کاربری خود شوید" },
+        { status: 401 }
+      );
+    }
+
+    const user = await User.findOne({ emial: session.user.email });
+    if (!user) {
+      return NextResponse.json(
+        { error: "حساب کاربری یافت نشد" },
+        { status: 404 }
+      );
+    }
+
+    const profile = await Profile.findOne({ _id: id });
+    if (!user._id.equals(profile.userId)) {
+      return NextResponse.json(
+        { error: "دسترسی شما به این آگهی محدود شده است" },
+        { status: 403 }
+      );
+    }
+
+    await profile.deleteOne({ _id: id });
+
+    return NextResponse.json(
+      { message: "آگهی موردنظر حذف شد" },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { error: "مشکلی در سرور رخ داده است" },
+      { status: 500 }
+    );
+  }
+}
