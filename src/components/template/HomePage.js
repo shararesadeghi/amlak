84447156306aa8f45262e9eaec11e5f283899479
Jamal Shamsi <93354040+jamalshamsi1988@users.noreply.github.com>
2023-12-14 23:30:45 +0330

    categorycard for homepage created

--- src/components/template/HomePage.js
+++ src/components/template/HomePage.js
@@ -1,34 +1,42 @@
-import { FiCircle } from 'react-icons/fi';
-import styles from '@/template/HomePage.module.css';
+import { FiCircle } from "react-icons/fi";
+import styles from "@/template/HomePage.module.css";
+import CategoryCard from "@/module/CategoryCard";
 
 const HomePage = () => {
-    const services = ["خرید", "فروش", "رهن", "اجاره"];
-    const cities = [
-      "تهران",
-      "سنندج",
-      "کرمانشاه",
-      "اهواز",
-      "مشهد",
-      "اصفهان",
-      "شیراز",
-      "خرم آباد",
-    ];
+  const services = ["خرید", "فروش", "رهن", "اجاره"];
+  const cities = [
+    "تهران",
+    "سنندج",
+    "کرمانشاه",
+    "اهواز",
+    "مشهد",
+    "اصفهان",
+    "شیراز",
+    "خرم آباد",
+  ];
   return (
     <div>
-        <div className={styles.banner}>
-            <div className={styles.desc}>
-                <h1>سامانه خرید و اجاره ملک</h1>
-                <ul>
-                    {services.map(item=>
-                        <li key={item}>
-                            <FiCircle/>
-                            <span>{item}</span>
-                        </li>)}
-                </ul>
-            </div>
+      <div className={styles.banner}>
+        <div className={styles.desc}>
+          <h1>سامانه خرید و اجاره ملک</h1>
+          <ul>
+            {services.map((item) => (
+              <li key={item}>
+                <FiCircle />
+                <span>{item}</span>
+              </li>
+            ))}
+          </ul>
         </div>
+      </div>
+      <div className={styles.categories}>
+        <CategoryCard title="خانه ویلایی" name="villa" />
+        <CategoryCard title="آپارتمان" name="apartment" />
+        <CategoryCard title="مغازه" name="store" />
+        <CategoryCard title="دفتر" name="office" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
