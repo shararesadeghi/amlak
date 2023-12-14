5852ac94f37de2c913d18f37ac6595d2f66fc735
Jamal Shamsi <93354040+jamalshamsi1988@users.noreply.github.com>
2023-12-14 23:52:47 +0330

    add get api for profiles in profilePage

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -5,6 +5,21 @@ import { Types } from "mongoose";
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 
+export async function GET() {
+  try {
+    await connectDB();
+
+    const profiles = await Profile.find().select("-userId");
+    return NextResponse.json({ data: profiles }, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { error: "مشکلی در سرور رخ داده است" },
+      { status: 500 }
+    );
+  }
+}
+
 export async function POST(req) {
   try {
     await connectDB();
