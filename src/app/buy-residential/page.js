ac16f810f13a2e4c1f48f2e6e23238e89dc99234
Jamal Shamsi <93354040+jamalshamsi1988@users.noreply.github.com>
2023-12-15 00:10:10 +0330

    show profiles in buyresidential

--- src/app/buy-residential/page.js
+++ src/app/buy-residential/page.js
@@ -6,8 +6,10 @@ const BuyResidentials = async() => {
     const res = await fetch("http://localhost:3000/api/profile",{cache:"no-store"});
     const data = await res.json();
 
+    if(data.error) return <h3>مشکلی پیش آمده است</h3>
+
   return (
-    <BuyResidentialsPage data={data}/>
+    <BuyResidentialsPage data={data.data}/>
   )
 }
 
--- src/components/template/BuyResidentialsPage.js
+++ src/components/template/BuyResidentialsPage.js
@@ -1,9 +1,20 @@
-import styles from '@/template/BuyResidentialsPage.module.css';
+import Card from "@/module/Card";
+import styles from "@/template/BuyResidentialsPage.module.css";
 
-const BuyResidentialsPage = () => {
+const BuyResidentialsPage = ({ data }) => {
   return (
-    <div>BuyResidentialsPage</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.sidebar}></div>
+      <div className={styles.main}>
+        {data.length ? null : (
+          <p className={styles.text}>هیچ آگهی ثبت نشده است</p>
+        )}
+        {data.map((profile) => (
+          <Card key={profile._id} data={profile} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default BuyResidentialsPage
+export default BuyResidentialsPage;
