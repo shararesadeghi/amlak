87658dd7cb466048b99decbeb29d2be7ebb569a9
Jamal Shamsi <93354040+jamalshamsi1988@users.noreply.github.com>
2023-12-17 00:30:33 +0330

    sidebar for detailspage created

--- src/components/template/DetailsPage.js
+++ src/components/template/DetailsPage.js
@@ -8,38 +8,73 @@ import { GiOfficeChair } from "react-icons/gi";
 import styles from "@/template/DetailsPage.module.css";
 import Title from "@/module/Title";
 import ItemList from "@/module/ItemList";
+import { e2p, sp } from "@/utils/replaceNumber";
 
-const DetailsPage = ({data}) => {
-
+const DetailsPage = ({
+  data: {
+    title,
+    location,
+    description,
+    amenities,
+    rules,
+    realState,
+    phone,
+    price,
+    category,
+    constructionDate,
+  },
+}) => {
   const categories = {
-    apartment:"آپارتمان",
-    villa:"ویلا",
-    store:"مغازه",
-    office:"دفتر"
-  }
+    apartment: "آپارتمان",
+    villa: "ویلا",
+    store: "مغازه",
+    office: "دفتر",
+  };
 
-  const icons={
-    villa:<RiHome3Line/>,
-    apartment:<MdApartment/>,
-    store:<BiStore/>,
-    office:<GiOfficeChair/>
-  }
-  return <div className={styles.container}>
-    <div className={styles.main}>
-      <h1>{data.title}</h1>
-      <span>
-        <HiOutlineLocationMarker/>
-        {data.location}
-      </span>
-      <Title>توضیحات</Title>
-      <p>{data.description}</p>
-      <Title>امکانات رفاهی</Title>
-      <ItemList data={data.amenities}/>
-      <Title>قوانین</Title>
-      <ItemList data={data.rules}/>
+  const icons = {
+    villa: <RiHome3Line />,
+    apartment: <MdApartment />,
+    store: <BiStore />,
+    office: <GiOfficeChair />,
+  };
+  return (
+    <div className={styles.container}>
+      <div className={styles.main}>
+        <h1>{title}</h1>
+        <span>
+          <HiOutlineLocationMarker />
+          {location}
+        </span>
+        <Title>توضیحات</Title>
+        <p>{description}</p>
+        <Title>امکانات رفاهی</Title>
+        <ItemList data={amenities} />
+        <Title>قوانین</Title>
+        <ItemList data={rules} />
+      </div>
+      <div className={styles.sidebar}>
+        <div className={styles.realState}>
+          <SiHomebridge/>
+          <p>املاک {realState}</p>
+          <span>
+            <AiOutlinePhone/>
+            {e2p(phone)}
+          </span>
+        </div>
+        <div className={styles.price}>
+          <p>
+            {icons[category]}
+            {categories[category]}
+          </p>
+          <p>{sp(price)} تومان</p>
+          <p>
+            <BiCalendarCheck/>
+            {new Date(constructionDate).toLocaleDateString("fa-IR")}
+          </p>
+        </div>
+      </div>
     </div>
-    <div className={styles.sidebar}></div>
-  </div>;
+  );
 };
 
 export default DetailsPage;
