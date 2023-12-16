968971274a24f263e4031dc129ba605f38c291dd
Jamal Shamsi <93354040+jamalshamsi1988@users.noreply.github.com>
2023-12-17 00:11:55 +0330

    add main section to detailspage

---
+++ src/components/module/Title.js
@@ -0,0 +1,9 @@
+import styles from '@/module/Title.module.css';
+
+const Title = ({children}) => {
+  return (
+    <h3 className={styles.title}>{children}</h3>
+  )
+}
+
+export default Title
--- src/components/template/DetailsPage.js
+++ src/components/template/DetailsPage.js
@@ -1,12 +1,43 @@
 import { SiHomebridge } from "react-icons/si";
 import { AiOutlinePhone } from "react-icons/ai";
 import { HiOutlineLocationMarker } from "react-icons/hi";
-import { BiCalendarCheck } from "react-icons/bi";
+import { BiCalendarCheck, BiStore } from "react-icons/bi";
+import { RiHome3Line } from "react-icons/ri";
+import { MdApartment } from "react-icons/md";
+import { GiOfficeChair } from "react-icons/gi";
 import styles from "@/template/DetailsPage.module.css";
+import Title from "@/module/Title";
+import ItemList from "@/module/ItemList";
 
-const DetailsPage = () => {
+const DetailsPage = ({data}) => {
+
+  const categories = {
+    apartment:"آپارتمان",
+    villa:"ویلا",
+    store:"مغازه",
+    office:"دفتر"
+  }
+
+  const icons={
+    villa:<RiHome3Line/>,
+    apartment:<MdApartment/>,
+    store:<BiStore/>,
+    office:<GiOfficeChair/>
+  }
   return <div className={styles.container}>
-    <div className={styles.main}></div>
+    <div className={styles.main}>
+      <h1>{data.title}</h1>
+      <span>
+        <HiOutlineLocationMarker/>
+        {data.location}
+      </span>
+      <Title>توضیحات</Title>
+      <p>{data.description}</p>
+      <Title>امکانات رفاهی</Title>
+      <ItemList data={data.amenities}/>
+      <Title>قوانین</Title>
+      <ItemList data={data.rules}/>
+    </div>
     <div className={styles.sidebar}></div>
   </div>;
 };
