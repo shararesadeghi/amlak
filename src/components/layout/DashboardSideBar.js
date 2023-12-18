941d41d4bb46074a1c1e7d0db7d4fbf25fd2b397
Jamal Shamsi <93354040+jamalshamsi1988@users.noreply.github.com>
2023-12-18 16:17:43 +0330

    change dashboardsidebar for admin role

--- src/components/layout/DashboardSideBar.js
+++ src/components/layout/DashboardSideBar.js
@@ -1,22 +1,21 @@
 import LogoutButton from "@/module/LogoutButton";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import Link from "next/link";
 import { CgProfile } from "react-icons/cg";
 import styles from "@/layout/DashboardSideBar.module.css";
 
-const DashboardSideBar = async ({ children }) => {
-  const session = await getServerSession(authOptions);
+const DashboardSideBar = async ({ children, role, email }) => {
   return (
     <div className={styles.container}>
       <div className={styles.sidebar}>
         <CgProfile />
-        <p>{session?.user.email}</p>
+        {role === "ADMIN" ? "ادمین" : null}
+        <p>{email}</p>
         <span></span>
         <Link href="/dashboard">حساب کاربری</Link>
         <Link href="/dashboard/my-profiles">آگهی های من</Link>
         <Link href="/dashboard/add">ثبت آگهی</Link>
-        <LogoutButton/>
+        {role === "ADMIN" ? <Link href="/admin">درانتظار تایید</Link> : null}
+        <LogoutButton />
       </div>
       <div className={styles.main}>{children}</div>
     </div>
