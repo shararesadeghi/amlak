b6d535da0cca134a464e76d59926f39fa45bd579
Jamal Shamsi <93354040+jamalshamsi1988@users.noreply.github.com>
2023-12-18 16:10:02 +0330

    change dashboardlayout for manage user is user or admin

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,12 +1,22 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import {redirect} from 'next/navigation';
+import { redirect } from "next/navigation";
 import DashboardSideBar from "@/layout/DashboardSideBar";
+import connectDB from "@/utils/connectDB";
+import User from "@/models/User";
 
 const DashboardLayout = async ({ children }) => {
-    const session = await getServerSession(authOptions);
-    if(!session) redirect("/signin");
-  return <DashboardSideBar>{children}</DashboardSideBar>;
+  const session = await getServerSession(authOptions);
+  if (!session) redirect("/signin");
+
+  await connectDB();
+  const user = await User.findOne({ email: session.user.email });
+  if (!user) return <h3>مشکلی پیش آمده است</h3>;
+  return (
+    <DashboardSideBar role={user.role} email={user.email}>
+      {children}
+    </DashboardSideBar>
+  );
 };
 
 export default DashboardLayout;
